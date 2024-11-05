6de0d779b68320d94406d6ebb6c19280437ffd60
p-shrey10 <pradhanangashrey@gmail.com>
2024-11-05 22:41:40 +0545

    added router

---
+++ src/routers/Router.tsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import All from "../components/All";
+import Home from "../pages/Home/Home";
+import Service from "../pages/Service/Service";
+import ServiceDetail from "../pages/Service/ServiceDetail";
+import About from "../pages/About/About";
+import Project from "../pages/Project/Project";
+import ProjectDetail from "../pages/Project/ProjectDetail";
+import Partner from "../pages/Partner/Partner";
+import Team from "../pages/Team/Team";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <All />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/service",
+        element: <Service />,
+      },
+      {
+        path: "/service/:id",
+        element: <ServiceDetail />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/project",
+        element: <Project />,
+      },
+      {
+        path: "/project/:id",
+        element: <ProjectDetail />,
+      },
+      {
+        path: "/partner",
+        element: <Partner />,
+      },
+      {
+        path: "/team",
+        element: <Team />,
+      },
+      {
+        path: "/contact",
+        element: <h1>Contact</h1>,
+      },
+    ],
+  },
+]);
+
+export default router;
